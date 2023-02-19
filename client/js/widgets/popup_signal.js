8e00326fc518b848cd5d0eb431a91ecebe10e3de
MaksimSamedov <makssamedov36@mail.ru>
2023-02-19 09:47:26 +0300

    графики

--- client/js/widgets/popup_signal.js
+++ client/js/widgets/popup_signal.js
@@ -6,6 +6,7 @@ import App from '/client/js/app.js';
 export default class PopupSignalWidget extends PopupBaseWidget {
 
     #signal
+    #graphic
 
     constructor(signal) {
         if(!signal){
@@ -14,15 +15,59 @@ export default class PopupSignalWidget extends PopupBaseWidget {
         super();
         this.#signal = signal
         this.setTitle(signal.getName())
+        this.getContent().innerHTML = '<canvas id="myChart' + encodeURIComponent(this.#signal.getName()) + '"></canvas>'
+    }
+
+    initGraphic = () => {
+        const ctx = document.getElementById('myChart' + encodeURIComponent(this.#signal.getName()));
+
+        const info = this.#signal.getInfo()
+        const labels = Object.keys(info)
+        this.#graphic = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels.map(l => new Date(l)),
+                datasets: [{
+                    label: '',
+                    data: labels.map(l => info[l].value * 1),
+                    fill: false,
+                    borderColor: 'rgb(75, 192, 192)',
+                    tension: 0.1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+        console.log(this.#graphic)
+    }
+
+    updateGraphic = () => {
+        const info = this.#signal.getInfo()
+        console.log('updateGraphic', info)
+        const labels = Object.keys(info)
+        this.#graphic.data.labels = labels.map(l => new Date(l))
+        this.#graphic.data.datasets[0].data = labels.map(l => info[l].value * 1)
+        this.#graphic.update()
+        debugger
     }
 
     getClasses() {
         return ['popup', 'popup-signal']
     }
 
-    renderWidget({_updates}) {
-        const exhauster_updates = _updates[this.#signal.getExhausterId()] || []
-        const updates = exhauster_updates[this.#signal.getName()] || false
+    renderWidget({updates}) {
+
+        if(!this.#graphic){
+            this.initGraphic()
+        }
+        this.updateGraphic()
+        const exhauster_updates = updates[this.#signal.getExhausterId()] || []
+        updates = exhauster_updates[this.#signal.getName()] || false
         if(!updates){
             return
         }
@@ -32,7 +77,7 @@ export default class PopupSignalWidget extends PopupBaseWidget {
         //     children[key].renderWidget({signal_updates: exhauster_updates[key]})
         // })
 
-        this.getContent().innerHTML = 'popup.....'
+        // this.getContent().innerHTML = 'popup.....'
     }
 
 }
